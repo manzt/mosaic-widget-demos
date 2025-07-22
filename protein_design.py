48dc6b6cc3f30b2cb7e59cdc2a606032e0c539d1
Trevor Manz <trevor.j.manz@gmail.com>
2025-07-22 12:43:00 -0400

    initial commit

---
+++ protein_design.py
@@ -0,0 +1,215 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "marimo",
+#     "mosaic-widget==0.18.0",
+#     "polars==1.31.0",
+# ]
+# ///
+
+import marimo
+
+__generated_with = "0.14.12"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _(spec):
+    import marimo as mo
+    import polars as pl
+    from mosaic_widget import MosaicWidget
+
+    proteins = pl.read_parquet(
+        "https://github.com/uwdata/mosaic/raw/refs/heads/main/data/protein-design.parquet"
+    )
+
+    mo.ui.anywidget(MosaicWidget(spec, data={"proteins": proteins}))
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    spec = {
+        "meta": {
+            "title": "Protein Design Explorer",
+            "description": 'Explore synthesized proteins generated via\n[RFDiffusion](https://www.bakerlab.org/2023/07/11/diffusion-model-for-protein-design/).\n"Minibinders" are small proteins that bind to a specific protein target.\nWhen designing a minibinder, a researcher inputs the structure of the\ntarget protein and other parameters into the AI diffusion model. Often, a\nsingle, promising (parent) _version_ can be run through the model again to\nproduce additional, similar designs to better sample the design space.\n\nThe pipeline generates tens of thousands of protein designs. The metric\n_pAE_ (predicted alignment error) measures how accurate a model was at\npredicting the minibinder shape, whereas _pLDDT_ (predicted local distance\ndifference test) measures a model\'s confidence in minibinder structure\nprediction. For _pAE_ lower is better, for _pLDDT_ higher is better.\n\nAdditional parameters include _partial t_ to set the time steps used by\nthe model, _noise_ to create more diversity of designs, _gradient decay\nfunction_ and _gradient scale_ to guide prioritizing different positions\nat different time points, and _movement_ to denote whether the minibinder\nwas left in its original position ("og") or moved to a desirable position\n("moved").\n\nThe dashboard below enables exploration of the results to identify\npromising protein designs and assess the effects of process parameters.\n',
+            "credit": "Adapted from a [UW CSE 512](https://courses.cs.washington.edu/courses/cse512/24sp/) project by Christina Savvides, Alexander Shida, Riti Biswas, and Nora McNamara-Bordewick. Data from the [UW Institute for Protein Design](https://www.ipd.uw.edu/).\n",
+        },
+        "params": {
+            "query": {"select": "crossfilter"},
+            "point": {"select": "intersect", "empty": True},
+            "plddt_domain": [67, 94.5],
+            "pae_domain": [5, 29],
+            "scheme": "observable10",
+        },
+        "vconcat": [
+            {
+                "hconcat": [
+                    {
+                        "input": "menu",
+                        "from": "proteins",
+                        "column": "partial_t",
+                        "label": "Partial t",
+                        "as": "$query",
+                    },
+                    {
+                        "input": "menu",
+                        "from": "proteins",
+                        "column": "noise",
+                        "label": "Noise",
+                        "as": "$query",
+                    },
+                    {
+                        "input": "menu",
+                        "from": "proteins",
+                        "column": "gradient_decay_function",
+                        "label": "Gradient Decay",
+                        "as": "$query",
+                    },
+                    {
+                        "input": "menu",
+                        "from": "proteins",
+                        "column": "gradient_scale",
+                        "label": "Gradient Scale",
+                        "as": "$query",
+                    },
+                ]
+            },
+            {"vspace": "1.5em"},
+            {
+                "hconcat": [
+                    {
+                        "plot": [
+                            {
+                                "mark": "rectY",
+                                "data": {"from": "proteins", "filterBy": "$query"},
+                                "x": {"bin": "plddt_total", "steps": 60},
+                                "y": {"count": None},
+                                "z": "version",
+                                "fill": "version",
+                                "order": "z",
+                                "reverse": True,
+                                "insetLeft": 0.5,
+                                "insetRight": 0.5,
+                            }
+                        ],
+                        "width": 600,
+                        "height": 55,
+                        "xAxis": None,
+                        "yAxis": None,
+                        "xDomain": "$plddt_domain",
+                        "colorDomain": "Fixed",
+                        "colorScheme": "$scheme",
+                        "marginLeft": 40,
+                        "marginRight": 0,
+                        "marginTop": 0,
+                        "marginBottom": 0,
+                    },
+                    {"hspace": 5},
+                    {
+                        "legend": "color",
+                        "for": "scatter",
+                        "columns": 1,
+                        "as": "$query",
+                    },
+                ]
+            },
+            {
+                "hconcat": [
+                    {
+                        "name": "scatter",
+                        "plot": [
+                            {"mark": "frame", "stroke": "#ccc"},
+                            {
+                                "mark": "raster",
+                                "data": {"from": "proteins", "filterBy": "$query"},
+                                "x": "plddt_total",
+                                "y": "pae_interaction",
+                                "fill": "version",
+                                "pad": 0,
+                            },
+                            {
+                                "select": "intervalXY",
+                                "as": "$query",
+                                "brush": {
+                                    "stroke": "currentColor",
+                                    "fill": "transparent",
+                                },
+                            },
+                            {
+                                "mark": "dot",
+                                "data": {"from": "proteins", "filterBy": "$point"},
+                                "x": "plddt_total",
+                                "y": "pae_interaction",
+                                "fill": "version",
+                                "stroke": "currentColor",
+                                "strokeWidth": 0.5,
+                            },
+                        ],
+                        "opacityDomain": [0, 2],
+                        "opacityClamp": True,
+                        "colorDomain": "Fixed",
+                        "colorScheme": "$scheme",
+                        "xDomain": "$plddt_domain",
+                        "yDomain": "$pae_domain",
+                        "xLabelAnchor": "center",
+                        "yLabelAnchor": "center",
+                        "marginTop": 0,
+                        "marginLeft": 40,
+                        "marginRight": 0,
+                        "width": 600,
+                        "height": 450,
+                    },
+                    {
+                        "plot": [
+                            {
+                                "mark": "rectX",
+                                "data": {"from": "proteins", "filterBy": "$query"},
+                                "x": {"count": None},
+                                "y": {"bin": "pae_interaction", "steps": 60},
+                                "z": "version",
+                                "fill": "version",
+                                "order": "z",
+                                "reverse": True,
+                                "insetTop": 0.5,
+                                "insetBottom": 0.5,
+                            }
+                        ],
+                        "width": 55,
+                        "height": 450,
+                        "xAxis": None,
+                        "yAxis": None,
+                        "marginTop": 0,
+                        "marginLeft": 0,
+                        "marginRight": 0,
+                        "yDomain": "$pae_domain",
+                        "colorDomain": "Fixed",
+                        "colorScheme": "$scheme",
+                    },
+                ]
+            },
+            {"vspace": "1em"},
+            {
+                "input": "table",
+                "as": "$point",
+                "filterBy": "$query",
+                "from": "proteins",
+                "columns": [
+                    "version",
+                    "pae_interaction",
+                    "plddt_total",
+                    "noise",
+                    "gradient_decay_function",
+                    "gradient_scale",
+                    "movement",
+                ],
+                "width": 680,
+                "height": 215,
+            },
+        ],
+    }
+    return (spec,)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ weather.py
@@ -0,0 +1,170 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "marimo",
+#     "mosaic-widget==0.18.0",
+#     "polars==1.31.0",
+#     "pyyaml==6.0.2",
+# ]
+# ///
+
+import marimo
+
+__generated_with = "0.14.12"
+app = marimo.App(width="medium")
+
+
+@app.cell(hide_code=True)
+def _(mo, w):
+    _weather =  w.params["click"].get("value")
+    _range = w.params["range"].get("value")
+
+    if _range:
+        start = _range[0].split("T")[0]
+        end = _range[1].split("T")[0]
+    else:
+        start = None
+        end = None
+
+    if _weather:
+        _weather = [w[0] for w in _weather]
+
+    mo.vstack(
+        [
+            mo.md(f"**Click selection**: `{_weather}`"),
+            mo.md(f"**Range selection**: `{start=}, {end=}`"),
+        ]
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(spec):
+    import marimo as mo
+    import polars as pl
+    import yaml
+
+    from mosaic_widget import MosaicWidget
+
+    weather = pl.read_csv(
+        "https://uwdata.github.io/mosaic-datasets/data/seattle-weather.csv",
+        try_parse_dates=True,
+    )
+
+    w = mo.ui.anywidget(MosaicWidget(spec, data={"weather": weather}))
+    w
+    return mo, pl, w, weather
+
+
+@app.cell(hide_code=True)
+def _(pl, w, weather):
+    _filtered = weather
+
+    categories = w.params["click"].get("value")
+    range = w.params["range"].get("value")
+
+    subset = weather
+    if range:
+        subset = subset.filter(
+            pl.col("date").ge(
+                pl.lit(range[0]).str.strptime(pl.Datetime).dt.date()
+            ),
+            pl.col("date").le(
+                pl.lit(range[1]).str.strptime(pl.Datetime).dt.date()
+            ),
+        )
+
+    if categories:
+        subset = subset.filter(
+            pl.col("weather").is_in(categories[0]),
+        )
+
+
+    subset
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    spec = {
+        "meta": {
+            "title": "Seattle Weather",
+            "description": "An interactive view of Seattle's weather, including maximum temperature, amount of precipitation, and type of weather. By dragging on the scatter plot, you can see the proportion of days in that range that have sun, fog, drizzle, rain, or snow.\n",
+            "credit": "Based on a [Vega-Lite/Altair example](https://vega.github.io/vega-lite/examples/interactive_seattle_weather.html) by Jake Vanderplas.",
+        },
+        "params": {
+            "click": {"select": "single"},
+            "domain": ["sun", "fog", "drizzle", "rain", "snow"],
+            "colors": ["#e7ba52", "#a7a7a7", "#aec7e8", "#1f77b4", "#9467bd"],
+        },
+        "vconcat": [
+            {
+                "hconcat": [
+                    {
+                        "plot": [
+                            {
+                                "mark": "dot",
+                                "data": {"from": "weather", "filterBy": "$click"},
+                                "x": {"dateMonthDay": "date"},
+                                "y": "temp_max",
+                                "fill": "weather",
+                                "r": "precipitation",
+                                "fillOpacity": 0.7,
+                            },
+                            {
+                                "select": "intervalX",
+                                "as": "$range",
+                                "brush": {"fill": "none", "stroke": "#888"},
+                            },
+                            {
+                                "select": "highlight",
+                                "by": "$range",
+                                "fill": "#ccc",
+                                "fillOpacity": 0.2,
+                            },
+                            {"legend": "color", "as": "$click", "columns": 1},
+                        ],
+                        "xyDomain": "Fixed",
+                        "xTickFormat": "%b",
+                        "colorDomain": "$domain",
+                        "colorRange": "$colors",
+                        "rDomain": "Fixed",
+                        "rRange": [2, 10],
+                        "width": 680,
+                        "height": 300,
+                    }
+                ]
+            },
+            {
+                "plot": [
+                    {
+                        "mark": "barX",
+                        "data": {"from": "weather"},
+                        "x": {"count": None},
+                        "y": "weather",
+                        "fill": "#ccc",
+                        "fillOpacity": 0.2,
+                    },
+                    {
+                        "mark": "barX",
+                        "data": {"from": "weather", "filterBy": "$range"},
+                        "x": {"count": None},
+                        "y": "weather",
+                        "fill": "weather",
+                    },
+                    {"select": "toggleY", "as": "$click"},
+                    {"select": "highlight", "by": "$click"},
+                ],
+                "xDomain": "Fixed",
+                "yDomain": "$domain",
+                "yLabel": None,
+                "colorDomain": "$domain",
+                "colorRange": "$colors",
+            },
+        ],
+    }
+    return (spec,)
+
+
+if __name__ == "__main__":
+    app.run()
